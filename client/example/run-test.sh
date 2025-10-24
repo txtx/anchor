0be88255fe1e900cc7d1b8add60b20b983eddfeb
MicaiahReid <micaiahreid@gmail.com>
2025-10-24 13:54:37 -0400

    disable check_surfpool call in run-test.sh

--- client/example/run-test.sh
+++ client/example/run-test.sh
@@ -142,11 +142,11 @@ start_surfpool() {
         --input basic_4_pid=$basic_4_pid \
         --input events_pid=$events_pid \
         --input optional_pid=$optional_pid
-        
+
     sleep 3
 
     echo "Surfpool PID: $surfpool_pid"
-    check_surfpool $surfpool_pid
+    # check_surfpool $surfpool_pid
 
     echo $surfpool_pid
 }
